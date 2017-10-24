360b61b305e9a98f163563c29065042195c5c24c
oprock <31218117+oprock@users.noreply.github.com>
2017-10-24 13:35:57 -0400

    Initial Upload

---
+++ DutyRoster1/src/dutyroster1/FXMLDocumentController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dutyroster1;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author othen
+ */
+public class FXMLDocumentController implements Initializable {
+    
+   
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
