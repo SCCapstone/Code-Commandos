261ce395047a957ee1bdd564d380249b890090bb
Michael Harlow <harlowm@email.usca.edu>
2017-10-30 01:36:57 -0400

    fix

--- DutyRoster1/src/dutyroster1/DutyRoster1.java
+++ DutyRoster1/src/dutyroster1/DutyRoster1.java
@@ -43,8 +43,6 @@ public class DutyRoster1 extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        long b=5;
-        int a = 1;
         launch(args);
     }
     
