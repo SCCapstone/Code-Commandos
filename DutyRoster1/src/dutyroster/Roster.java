32086661bfaccadbfc3d545499749e77532fb04e
Michael Harlow <harlowm@email.usca.edu>
2018-03-22 13:38:16 -0400

    Adding a second interval calculator for rest period

--- DutyRoster1/src/dutyroster/Roster.java
+++ DutyRoster1/src/dutyroster/Roster.java
@@ -1,34 +1,33 @@
 /**
- * @authors Othen Prock 
- * @version 1
- * 2/10/18
+ * @authors Othen Prock, Michael Harlow
+ * @version 2
+ * 3/22/18
  */
 
 package dutyroster;
-
-import java.util.ArrayList;
-
-//This clas is used as a secure strut
 public class Roster {
 
     
     private String title;
-    private int interval, amount;
+    private int dInterval; // Duty Interval
+    private int rInterval; // Rest Interval
+    private int amount;
     private boolean weekends, holidays;
     
     //Constructors
     public Roster() {
-      this("New Roster",24,1,true,true);
+      this("New Roster",24,24,1,true,true);
     }
     
     public Roster(String title) {
-      this(title,24,1,true,true);
+      this(title,24,24,1,true,true);
     } 
     
-    public Roster(String title, int interval, int amount, boolean weekends, boolean holidays) {
+    public Roster(String title, int dInterval, int rInterval, int amount, boolean weekends, boolean holidays) {
         
         this.title = title;
-        this.interval = interval;
+        this.dInterval = dInterval;
+        this.rInterval = rInterval;
         this.amount = amount;
         this.weekends = weekends;
         this.holidays = holidays;
@@ -39,8 +38,13 @@ public class Roster {
         title = val;
     }
       
-     public void setInterval(int val) {
-        interval = val;
+    public void setDInterval(int val) {
+        dInterval = val;
+    }
+    
+    
+    public void setRInterval(int val) {
+        rInterval = val;
     }
      
     public void setAmount(int val) {
@@ -59,10 +63,14 @@ public class Roster {
         return title;
     }
     
-    public int getInterval(){
-        return interval;
+    public int getDInterval(){
+        return dInterval;
     }
-    
+     
+    public int getRInterval(){
+        return rInterval;
+    }
+        
     public int getAmount(){
         return amount;
     }
